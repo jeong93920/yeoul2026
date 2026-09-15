f84b739b947ba4480585a0558544b04986879419
고예훈 <goyehun@goyehun-ui-MacBookPro.local>
2026-09-16 08:59:18 +0900

    fix: add booth menu images

---
+++ supabase/migrations/20260916090500_update_menu_images.sql
@@ -0,0 +1,11 @@
+update public.booth_menu_items
+set image_url = 'https://vedraqrdkwyyipztxddw.supabase.co/storage/v1/object/public/booth-menu-images/potato-cheese-nurungji.jpeg'
+where name = '감자치즈누룽지';
+
+update public.booth_menu_items
+set image_url = 'https://vedraqrdkwyyipztxddw.supabase.co/storage/v1/object/public/booth-menu-images/kimchi-noodles.jpeg'
+where name = '김치말이국수';
+
+update public.booth_menu_items
+set image_url = 'https://vedraqrdkwyyipztxddw.supabase.co/storage/v1/object/public/booth-menu-images/grape-ade.jpeg'
+where name = '청포도에이드';
